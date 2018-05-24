e3325d53651f4bd7d0c75427d15cd1317bfd76e8
Tomasz Gorochowik <tgorochowik@antmicro.com>
2018-05-24 10:23:48 +0200

    net: gptp: Fix validation of non-numeric inputs from net shell
    The shell did not check if the gptp port (command argument) is numeric.
    Add that check before executing the port info functions.
    
    Signed-off-by: Tomasz Gorochowik <tgorochowik@antmicro.com>

--- subsys/net/ip/net_shell.c
+++ subsys/net/ip/net_shell.c
@@ -2098,9 +2098,14 @@ int net_shell_cmd_gptp(int argc, char *argv[])
 #endif /* CONFIG_NET_GPTP_STATISTICS */
 
 	} else if (argv[arg]) {
-		int port = strtol(argv[arg], NULL, 10);
+		char *endptr;
+		int port = strtol(argv[arg], &endptr, 10);
 
-		gptp_print_port_info(port);
+		if (endptr != argv[arg]) {
+			gptp_print_port_info(port);
+		} else {
+			printk("Not a valid gPTP port number: %s\n", argv[arg]);
+		}
 	} else {
 		gptp_foreach_port(gptp_port_cb, &count);
 
