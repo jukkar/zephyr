938b6fa83f2d125d1d9a0d81605c97758c4f7a57
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2020-10-08 14:03:02 +0300

    drivers: gsm: Fix the device type
    The devices are now const so fix the prototype in header file.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- include/drivers/gsm_ppp.h
+++ include/drivers/gsm_ppp.h
@@ -8,7 +8,7 @@
 #define GSM_PPP_H_
 
 struct device;
-void gsm_ppp_start(struct device *device);
-void gsm_ppp_stop(struct device *device);
+void gsm_ppp_start(const struct device *device);
+void gsm_ppp_stop(const struct device *device);
 
 #endif /* GSM_PPP_H_ */
