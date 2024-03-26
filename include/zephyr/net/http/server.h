e756d9525d4bd9cfc0962dbce330cb3268f289e6
Robert Lubos <robert.lubos@nordicsemi.no>
2024-03-26 16:51:01 +0100

    net: lib: http_server: Make sure preface is sent only once
    In case of HTTP/1 to HTTP/2 upgrade, the server preface is sent directly
    after replying with 101 Switching Protocols response (it has to be the
    first frame on the stream). Therefore, it's no longer needed to send the
    preface again, after receiving the client preface.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- include/zephyr/net/http/server.h
+++ include/zephyr/net/http/server.h
@@ -125,6 +125,7 @@ struct http_frame {
 struct http_client_ctx {
 	int fd;
 	int offset;
+	bool preface_sent;
 	bool has_upgrade_header;
 	unsigned char buffer[HTTP_SERVER_CLIENT_BUFFER_SIZE];
 	enum http_server_state server_state;
