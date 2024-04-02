88c009f37184ecbe088c083026398436d29bef51
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2024-04-02 15:22:25 +0300

    tests: net: http_server: Fix the tests
    Fixing the tests so that CI passes.
    Removed json tests as json handling belong to application.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- tests/net/lib/http_server/crime/src/main.c
+++ tests/net/lib/http_server/crime/src/main.c
@@ -29,20 +29,30 @@ static const unsigned char index_html_gz[] = {
 };
 
 static const unsigned char compressed_inc_file[] = {
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x35, 0x8e, 0xc1, 0x0a, 0xc2,
-	0x30, 0x0c, 0x86, 0xef, 0x7d, 0x8a, 0xec, 0x05, 0x2c, 0xbb, 0x87, 0x5c, 0x54, 0xf0, 0xe0,
-	0x50, 0x58, 0x41, 0x3c, 0x4e, 0x17, 0x69, 0x21, 0xa5, 0x65, 0x2d, 0x42, 0xdf, 0xde, 0xba,
-	0x6e, 0x21, 0x10, 0xf8, 0xf9, 0xbe, 0x9f, 0x60, 0x77, 0xba, 0x1d, 0xcd, 0xf3, 0x7e, 0x06,
-	0x9b, 0xbd, 0x90, 0xc2, 0xfd, 0xf0, 0x34, 0x93, 0x82, 0x3a, 0x98, 0x5d, 0x16, 0xa6, 0xa1,
-	0xc0, 0xe8, 0x7c, 0x14, 0x86, 0x91, 0x97, 0x2f, 0x2f, 0xa8, 0x5b, 0xae, 0x50, 0x37, 0x16,
-	0x5f, 0x61, 0x2e, 0x9b, 0x62, 0x7b, 0x7a, 0xb0, 0xbc, 0x83, 0x67, 0xc8, 0x01, 0x7c, 0x81,
-	0xd4, 0xd4, 0xb4, 0xaa, 0x5d, 0x55, 0xfa, 0x8d, 0x8c, 0x64, 0xac, 0x4b, 0x50, 0x77, 0xda,
-	0xa1, 0x8b, 0x19, 0xae, 0xf0, 0x71, 0xc2, 0x07, 0xd4, 0xf1, 0xdf, 0xdf, 0x8a, 0xab, 0xb4,
-	0xbe, 0xf6, 0x03, 0xea, 0x2d, 0x11, 0x5c, 0xb2, 0x00,
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x02, 0xff, 0x35, 0x8e, 0xc1, 0x0a, 0xc2, 0x30,
+	0x0c, 0x86, 0xef, 0x7d, 0x8a, 0xec, 0x05, 0x2c,
+	0xbb, 0x87, 0x5c, 0x54, 0xf0, 0xe0, 0x50, 0x58,
+	0x41, 0x3c, 0x4e, 0x17, 0x69, 0x21, 0xa5, 0x65,
+	0x2d, 0x42, 0xdf, 0xde, 0xba, 0x6e, 0x21, 0x10,
+	0xf8, 0xf9, 0xbe, 0x9f, 0x60, 0x77, 0xba, 0x1d,
+	0xcd, 0xf3, 0x7e, 0x06, 0x9b, 0xbd, 0x90, 0xc2,
+	0xfd, 0xf0, 0x34, 0x93, 0x82, 0x3a, 0x98, 0x5d,
+	0x16, 0xa6, 0xa1, 0xc0, 0xe8, 0x7c, 0x14, 0x86,
+	0x91, 0x97, 0x2f, 0x2f, 0xa8, 0x5b, 0xae, 0x50,
+	0x37, 0x16, 0x5f, 0x61, 0x2e, 0x9b, 0x62, 0x7b,
+	0x7a, 0xb0, 0xbc, 0x83, 0x67, 0xc8, 0x01, 0x7c,
+	0x81, 0xd4, 0xd4, 0xb4, 0xaa, 0x5d, 0x55, 0xfa,
+	0x8d, 0x8c, 0x64, 0xac, 0x4b, 0x50, 0x77, 0xda,
+	0xa1, 0x8b, 0x19, 0xae, 0xf0, 0x71, 0xc2, 0x07,
+	0xd4, 0xf1, 0xdf, 0xdf, 0x8a, 0xab, 0xb4, 0xbe,
+	0xf6, 0x03, 0xea, 0x2d, 0x11, 0x5c, 0xb2, 0x00,
+	0x00, 0x00,
 };
 
-static uint16_t test_http_service_port = htons(SERVER_PORT);
-HTTP_SERVICE_DEFINE(test_http_service, CONFIG_NET_CONFIG_MY_IPV4_ADDR, &test_http_service_port, 1,
+static uint16_t test_http_service_port = SERVER_PORT;
+HTTP_SERVICE_DEFINE(test_http_service, MY_IPV4_ADDR,
+		    &test_http_service_port, 1,
 		    10, NULL);
 
 struct http_resource_detail_static index_html_gz_resource_detail = {
@@ -70,90 +80,95 @@ static void server_thread_fn(void *arg0, void *arg1, void *arg2)
 
 static void test_crime(void)
 {
-	int r;
+	int ret, recv_len;
 	int client_fd;
 	int proto = IPPROTO_TCP;
-	char *addrstrp;
+	char *ptr;
 	const char *data;
 	size_t len;
 	k_tid_t server_thread_id;
 	struct sockaddr_in sa;
-	unsigned char addrstr[512];
+	static unsigned char buf[512];
+	static struct http_server_ctx ctx;
 
 	k_sem_init(&server_sem, 0, 1);
 
-	struct http_server_ctx ctx;
+	ret = http_server_init(&ctx);
+	zassert_equal(ret, 0, "Failed to create server socket (%d)", ret);
 
-	int server_fd = http_server_init(&ctx);
+	server_thread_id = k_thread_create(&server_thread, server_stack,
+					   K_THREAD_STACK_SIZEOF(server_stack),
+					   server_thread_fn,
+					   &ctx, NULL, NULL,
+					   K_PRIO_PREEMPT(8), 0, K_NO_WAIT);
 
-	zassert_true(server_fd >= 0, "Failed to create server socket");
-
-	server_thread_id =
-		k_thread_create(&server_thread, server_stack, STACK_SIZE, server_thread_fn, &ctx,
-				NULL, NULL, K_PRIO_PREEMPT(8), 0, K_NO_WAIT);
-
-	r = k_sem_take(&server_sem, K_MSEC(TIMEOUT));
-	zassert_equal(0, r, "failed to synchronize with server thread (%d)", r);
+	ret = k_sem_take(&server_sem, K_MSEC(TIMEOUT));
+	zassert_equal(0, ret, "failed to synchronize with server thread (%d)", ret);
 
 	k_thread_name_set(k_current_get(), "client");
 
-	r = socket(AF_INET, SOCK_STREAM, proto);
-	zassert_not_equal(r, -1, "failed to create client socket (%d)", errno);
-	client_fd = r;
+	ret = zsock_socket(AF_INET, SOCK_STREAM, proto);
+	zassert_not_equal(ret, -1, "failed to create client socket (%d)", errno);
+	client_fd = ret;
 
 	sa.sin_family = AF_INET;
 	sa.sin_port = htons(SERVER_PORT);
 
-	r = inet_pton(AF_INET, MY_IPV4_ADDR, &sa.sin_addr.s_addr);
-	zassert_not_equal(-1, r, "inet_pton() failed (%d)", errno);
-	zassert_not_equal(0, r, "%s is not a valid IPv4 address", MY_IPV4_ADDR);
-	zassert_equal(1, r, "inet_pton() failed to convert %s", MY_IPV4_ADDR);
+	ret = zsock_inet_pton(AF_INET, MY_IPV4_ADDR, &sa.sin_addr.s_addr);
+	zassert_not_equal(-1, ret, "inet_pton() failed (%d)", errno);
+	zassert_not_equal(0, ret, "%s is not a valid IPv4 address", MY_IPV4_ADDR);
+	zassert_equal(1, ret, "inet_pton() failed to convert %s", MY_IPV4_ADDR);
 
-	memset(addrstr, '\0', sizeof(addrstr));
-	addrstrp = (char *)inet_ntop(AF_INET, &sa.sin_addr, addrstr, sizeof(addrstr));
-	zassert_not_equal(addrstrp, NULL, "inet_ntop() failed (%d)", errno);
+	memset(buf, '\0', sizeof(buf));
+	ptr = (char *)zsock_inet_ntop(AF_INET, &sa.sin_addr, buf, sizeof(buf));
+	zassert_not_equal(ptr, NULL, "inet_ntop() failed (%d)", errno);
 
-	r = connect(client_fd, (struct sockaddr *)&sa, sizeof(sa));
-	zassert_not_equal(r, -1, "failed to connect (%d)", errno);
+	ret = zsock_connect(client_fd, (struct sockaddr *)&sa, sizeof(sa));
+	zassert_not_equal(ret, -1, "failed to connect (%s/%d)", strerror(errno), errno);
 
 	char *http1_request = "GET / HTTP/1.1\r\n"
 			      "Host: 127.0.0.1:8080\r\n"
-			      "User-Agent: curl/7.68.0\r\n"
 			      "Accept: */*\r\n"
 			      "Accept-Encoding: deflate, gzip, br\r\n"
 			      "\r\n";
 
-	r = send(client_fd, http1_request, strlen(http1_request), 0);
-	zassert_not_equal(r, -1, "send() failed (%d)", errno);
+	ret = zsock_send(client_fd, http1_request, strlen(http1_request), 0);
+	zassert_not_equal(ret, -1, "send() failed (%d)", errno);
 
-	memset(addrstr, 0, sizeof(addrstr));
-	r = recv(client_fd, addrstr, sizeof(addrstr), 0);
-	zassert_not_equal(r, -1, "recv() failed (%d)", errno);
+	memset(buf, 0, sizeof(buf));
+	recv_len = zsock_recv(client_fd, buf, sizeof(buf), 0);
+	zassert_not_equal(recv_len, -1, "recv() failed (%d)", errno);
 
-	memset(addrstr, 0, sizeof(addrstr));
-	r = recv(client_fd, addrstr, sizeof(addrstr), 0);
-	zassert_not_equal(r, -1, "recv() failed (%d)", errno);
-
-	data = addrstr;
 	len = sizeof(index_html_gz);
-	len = len - 2;
+
+	while (recv_len < len) {
+		ret = zsock_recv(client_fd, buf + recv_len, sizeof(buf) - recv_len, 0);
+		zassert_not_equal(ret, -1, "recv() failed (%d)", errno);
+
+		recv_len += ret;
+	}
+
+	data = strstr(buf, "\r\n\r\n");
+	zassert_not_null(data, "Header not found");
+
+	data += 4;
 
 	zassert_equal(len, sizeof(compressed_inc_file), "Invalid compressed file size");
 
-	int result = memcmp(data, compressed_inc_file, len);
+	ret = memcmp(data, compressed_inc_file, len);
+	zassert_equal(ret, 0,
+		      "inc_file and compressed_inc_file contents are not identical (%d)", ret);
 
-	zassert_equal(result, 0, "inc_file and compressed_inc_file contents are not identical");
-
-	r = close(client_fd);
-	zassert_not_equal(-1, r, "close() failed on the client fd (%d)", errno);
+	ret = zsock_close(client_fd);
+	zassert_not_equal(-1, ret, "close() failed on the client fd (%d)", errno);
 
 	http_server_stop(&ctx);
 
-	r = k_thread_join(&server_thread, K_FOREVER);
-	zassert_equal(0, r, "k_thread_join() failed (%d)", r);
+	ret = k_thread_join(&server_thread, K_FOREVER);
+	zassert_equal(0, ret, "k_thread_join() failed (%d)", ret);
 
-	r = close(server_fd);
-	zassert_not_equal(-1, r, "close() failed on the server fd (%d)", errno);
+	ret = http_server_cleanup(&ctx);
+	zassert_equal(ret, 0, "Failed to cleanup server (%d)", ret);
 }
 
 ZTEST(framework_tests_crime, test_gen_gz_inc_file)
--- tests/net/lib/http_server/tls/src/main.c
+++ tests/net/lib/http_server/tls/src/main.c
@@ -15,14 +15,7 @@
 #include <zephyr/sys/util.h>
 #include <zephyr/ztest.h>
 
-LOG_MODULE_REGISTER(tls_test, CONFIG_NET_SOCKETS_LOG_LEVEL);
-
-#define SECRET "framework_tests_tls"
-
-/**
- * @brief Size of the encrypted message passed between server and client.
- */
-#define SECRET_SIZE (sizeof(SECRET) - 1)
+LOG_MODULE_REGISTER(net_test, CONFIG_NET_SOCKETS_LOG_LEVEL);
 
 /** @brief Stack size for the server thread */
 #define STACK_SIZE 8192
@@ -40,11 +33,24 @@ static struct k_sem server_sem;
 static K_THREAD_STACK_DEFINE(server_stack, STACK_SIZE);
 static struct k_thread server_thread;
 
-static uint16_t test_http_service_port = htons(SERVER_PORT);
-HTTP_SERVICE_DEFINE(test_http_service, CONFIG_NET_CONFIG_MY_IPV4_ADDR, &test_http_service_port, 1,
-			10, NULL);
+enum tls_tag {
+	/** The Certificate Authority public key */
+	CA_CERTIFICATE_TAG,
+	/** Used for both the public and private server keys */
+	SERVER_CERTIFICATE_TAG,
+	/** Used for both the public and private client keys */
+	CLIENT_CERTIFICATE_TAG,
+};
 
-#ifdef CONFIG_TLS_CREDENTIALS
+static const sec_tag_t server_tag_list_verify[] = {
+	CA_CERTIFICATE_TAG,
+	SERVER_CERTIFICATE_TAG,
+};
+
+static uint16_t test_http_service_port = SERVER_PORT;
+HTTPS_SERVICE_DEFINE(test_http_service, MY_IPV4_ADDR, &test_http_service_port,
+		     1, 10, NULL, server_tag_list_verify,
+		     sizeof(server_tag_list_verify));
 
 static const unsigned char ca[] = {
 #include "ca.inc"
@@ -85,56 +91,74 @@ static const unsigned char client[] = {
 static const unsigned char client_privkey[] = {
 #include "client_privkey.inc"
 };
-#else /* CONFIG_TLS_CREDENTIALS */
-#define ca             NULL
-#define server         NULL
-#define server_privkey NULL
-#define client         NULL
-#define client_privkey NULL
-#endif /* CONFIG_TLS_CREDENTIALS */
+
+static const unsigned char index_html_gz[] = {
+#include "index.html.gz.inc"
+};
+
+static const unsigned char compressed_inc_file[] = {
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x02, 0xff, 0x35, 0x8e, 0xc1, 0x0a, 0xc2, 0x30,
+	0x0c, 0x86, 0xef, 0x7d, 0x8a, 0xec, 0x05, 0x2c,
+	0xbb, 0x87, 0x5c, 0x54, 0xf0, 0xe0, 0x50, 0x58,
+	0x41, 0x3c, 0x4e, 0x17, 0x69, 0x21, 0xa5, 0x65,
+	0x2d, 0x42, 0xdf, 0xde, 0xba, 0x6e, 0x21, 0x10,
+	0xf8, 0xf9, 0xbe, 0x9f, 0x60, 0x77, 0xba, 0x1d,
+	0xcd, 0xf3, 0x7e, 0x06, 0x9b, 0xbd, 0x90, 0xc2,
+	0xfd, 0xf0, 0x34, 0x93, 0x82, 0x3a, 0x98, 0x5d,
+	0x16, 0xa6, 0xa1, 0xc0, 0xe8, 0x7c, 0x14, 0x86,
+	0x91, 0x97, 0x2f, 0x2f, 0xa8, 0x5b, 0xae, 0x50,
+	0x37, 0x16, 0x5f, 0x61, 0x2e, 0x9b, 0x62, 0x7b,
+	0x7a, 0xb0, 0xbc, 0x83, 0x67, 0xc8, 0x01, 0x7c,
+	0x81, 0xd4, 0xd4, 0xb4, 0xaa, 0x5d, 0x55, 0xfa,
+	0x8d, 0x8c, 0x64, 0xac, 0x4b, 0x50, 0x77, 0xda,
+	0xa1, 0x8b, 0x19, 0xae, 0xf0, 0x71, 0xc2, 0x07,
+	0xd4, 0xf1, 0xdf, 0xdf, 0x8a, 0xab, 0xb4, 0xbe,
+	0xf6, 0x03, 0xea, 0x2d, 0x11, 0x5c, 0xb2, 0x00,
+	0x00, 0x00,
+};
+
+struct http_resource_detail_static index_html_gz_resource_detail = {
+	.common = {
+			.type = HTTP_RESOURCE_TYPE_STATIC,
+			.bitmask_of_supported_http_methods = BIT(HTTP_GET),
+		},
+	.static_data = index_html_gz,
+	.static_data_len = sizeof(index_html_gz),
+};
+
+HTTP_RESOURCE_DEFINE(index_html_gz_resource, test_http_service, "/",
+		     &index_html_gz_resource_detail);
 
 static void server_thread_fn(void *arg0, void *arg1, void *arg2)
 {
 	struct http_server_ctx *ctx = (struct http_server_ctx *)arg0;
 
-	int r;
-	int client_fd;
-	socklen_t addrlen;
-	char addrstr[BUFFER_SIZE];
-	struct sockaddr_in sa;
-
 	k_thread_name_set(k_current_get(), "server");
-
-	memset(&sa, 0, sizeof(sa));
-	addrlen = sizeof(sa);
 
 	k_sem_give(&server_sem);
 
-	client_fd = accept_new_client(ctx->server_fd);
-
-	NET_DBG("calling recv()");
-	r = recv(client_fd, addrstr, sizeof(addrstr), 0);
-	zassert_not_equal(r, -1, "recv() failed (%d)", errno);
-	zassert_equal(r, SECRET_SIZE, "expected: %zu actual: %d", SECRET_SIZE, r);
-
-	NET_DBG("calling send()");
-	r = send(client_fd, SECRET, SECRET_SIZE, 0);
-	zassert_not_equal(r, -1, "send() failed (%d)", errno);
-	zassert_equal(r, SECRET_SIZE, "expected: %zu actual: %d", SECRET_SIZE, r);
-
-	r = close(client_fd);
-	zassert_not_equal(r, -1, "close() failed on the server fd (%d)", errno);
+	http_server_start(ctx);
 }
 
 static void test_tls(void)
 {
-	int r;
+	int ret, recv_len;
 	int client_fd;
 	int proto = IPPROTO_TCP;
-	char *addrstrp;
+	size_t len;
+	char *ptr;
+	const char *data;
 	k_tid_t server_thread_id;
 	struct sockaddr_in sa;
-	char addrstr[256];
+	static unsigned char buf[512];
+	static struct http_server_ctx ctx;
+	char http1_request[] =
+		"GET / HTTP/1.1\r\n"
+		"Host: 127.0.0.1:8080\r\n"
+		"Accept: */*\r\n"
+		"Accept-Encoding: deflate, gzip, br\r\n"
+		"\r\n";
 
 	k_sem_init(&server_sem, 0, 1);
 
@@ -143,81 +167,98 @@ static void test_tls(void)
 		proto = IPPROTO_TLS_1_2;
 	}
 
-	struct http_server_ctx ctx;
+	ret = http_server_init(&ctx);
+	zassert_equal(ret, 0, "Failed to create server socket (%d)");
 
-	int server_fd = http_server_init(&ctx);
+	server_thread_id = k_thread_create(&server_thread, server_stack,
+					   K_THREAD_STACK_SIZEOF(server_stack),
+					   server_thread_fn,
+					   &ctx, NULL, NULL,
+					   K_PRIO_PREEMPT(8), 0, K_NO_WAIT);
 
-	zassert_true(server_fd >= 0, "Failed to create server socket");
-
-	server_thread_id =
-		k_thread_create(&server_thread, server_stack, STACK_SIZE, server_thread_fn, &ctx,
-				NULL, NULL, K_PRIO_PREEMPT(8), 0, K_NO_WAIT);
-
-	r = k_sem_take(&server_sem, K_MSEC(TIMEOUT));
-	zassert_equal(0, r, "failed to synchronize with server thread (%d)", r);
+	ret = k_sem_take(&server_sem, K_MSEC(TIMEOUT));
+	zassert_equal(ret, 0, "failed to synchronize with server thread (%d)", ret);
 
 	k_thread_name_set(k_current_get(), "client");
 
-	r = socket(AF_INET, SOCK_STREAM, proto);
-	zassert_not_equal(r, -1, "failed to create client socket (%d)", errno);
-	client_fd = r;
+	ret = zsock_socket(AF_INET, SOCK_STREAM, proto);
+	zassert_not_equal(ret, -1, "failed to create client socket (%d)", errno);
+	client_fd = ret;
 
-	if (IS_ENABLED(CONFIG_TLS_CREDENTIALS) && IS_ENABLED(CONFIG_NET_SOCKETS_SOCKOPT_TLS)) {
-
-		static const sec_tag_t client_tag_list_verify_none[] = {
-			HTTP_SERVER_CA_CERTIFICATE_TAG,
+	if (IS_ENABLED(CONFIG_TLS_CREDENTIALS) &&
+	    IS_ENABLED(CONFIG_NET_SOCKETS_SOCKOPT_TLS)) {
+		static const sec_tag_t sec_tag_list_verify_none[] = {
+			CA_CERTIFICATE_TAG,
 		};
-
 		const sec_tag_t *sec_tag_list;
 		size_t sec_tag_list_size;
 
-		sec_tag_list = client_tag_list_verify_none;
-		sec_tag_list_size = sizeof(client_tag_list_verify_none);
+		sec_tag_list_size = sizeof(sec_tag_list);
+		sec_tag_list = sec_tag_list_verify_none;
 
-		r = setsockopt(client_fd, SOL_TLS, TLS_SEC_TAG_LIST, sec_tag_list,
-			       sec_tag_list_size);
-		zassert_not_equal(r, -1, "failed to set TLS_SEC_TAG_LIST (%d)", errno);
+		ret = zsock_setsockopt(client_fd, SOL_TLS, TLS_SEC_TAG_LIST,
+				       sec_tag_list, sec_tag_list_size);
+		zassert_not_equal(ret, -1, "failed to set TLS_SEC_TAG_LIST (%d)", errno);
 
-		r = setsockopt(client_fd, SOL_TLS, TLS_HOSTNAME, "localhost", sizeof("localhost"));
-		zassert_not_equal(r, -1, "failed to set TLS_HOSTNAME (%d)", errno);
+		ret = zsock_setsockopt(client_fd, SOL_TLS, TLS_HOSTNAME,
+				       "localhost", sizeof("localhost"));
+		zassert_not_equal(ret, -1, "failed to set TLS_HOSTNAME (%d)", errno);
 	}
 
 	sa.sin_family = AF_INET;
 	sa.sin_port = htons(SERVER_PORT);
 
-	r = inet_pton(AF_INET, MY_IPV4_ADDR, &sa.sin_addr.s_addr);
-	zassert_not_equal(-1, r, "inet_pton() failed (%d)", errno);
-	zassert_not_equal(0, r, "%s is not a valid IPv4 address", MY_IPV4_ADDR);
-	zassert_equal(1, r, "inet_pton() failed to convert %s", MY_IPV4_ADDR);
+	ret = zsock_inet_pton(AF_INET, MY_IPV4_ADDR, &sa.sin_addr.s_addr);
+	zassert_not_equal(-1, ret, "inet_pton() failed (%d)", errno);
+	zassert_not_equal(ret, 0, "%s is not a valid IPv4 address", MY_IPV4_ADDR);
+	zassert_equal(ret, 1, "inet_pton() failed to convert %s", MY_IPV4_ADDR);
 
-	memset(addrstr, '\0', sizeof(addrstr));
-	addrstrp = (char *)inet_ntop(AF_INET, &sa.sin_addr, addrstr, sizeof(addrstr));
-	zassert_not_equal(addrstrp, NULL, "inet_ntop() failed (%d)", errno);
+	memset(buf, '\0', sizeof(buf));
+	ptr = (char *)zsock_inet_ntop(AF_INET, &sa.sin_addr, buf, sizeof(buf));
+	zassert_not_equal(ptr, NULL, "inet_ntop() failed (%d)", errno);
 
-	r = connect(client_fd, (struct sockaddr *)&sa, sizeof(sa));
-	zassert_not_equal(r, -1, "failed to connect (%d)", errno);
+	ret = connect(client_fd, (struct sockaddr *)&sa, sizeof(sa));
+	zassert_not_equal(ret, -1, "failed to connect (%d)", errno);
 
-	NET_DBG("Calling send()");
-	r = send(client_fd, SECRET, SECRET_SIZE, 0);
-	zassert_not_equal(r, -1, "send() failed (%d)", errno);
-	zassert_equal(SECRET_SIZE, r, "expected: %zu actual: %d", SECRET_SIZE, r);
+	ret = zsock_send(client_fd, http1_request, sizeof(http1_request) - 1, 0);
+	zassert_not_equal(ret, -1, "send() failed (%d)", errno);
+	zassert_equal(ret, sizeof(http1_request) - 1, "expected: %zu actual: %d",
+		      sizeof(http1_request) - 1, ret);
 
-	NET_DBG("Calling recv()");
-	memset(addrstr, 0, sizeof(addrstr));
-	r = recv(client_fd, addrstr, sizeof(addrstr), 0);
-	zassert_not_equal(r, -1, "recv() failed (%d)", errno);
-	zassert_equal(SECRET_SIZE, r, "expected: %zu actual: %d", SECRET_SIZE, r);
+	memset(buf, 0, sizeof(buf));
+	recv_len = zsock_recv(client_fd, buf, sizeof(buf), 0);
+	zassert_not_equal(recv_len, -1, "recv() failed (%d)", errno);
 
-	zassert_mem_equal(SECRET, addrstr, SECRET_SIZE, "expected: %s actual: %s", SECRET, addrstr);
+	len = sizeof(index_html_gz);
 
-	r = close(client_fd);
-	zassert_not_equal(-1, r, "close() failed on the client fd (%d)", errno);
+	while (recv_len < len) {
+		ret = zsock_recv(client_fd, buf + recv_len, sizeof(buf) - recv_len, 0);
+		zassert_not_equal(ret, -1, "recv() failed (%d)", errno);
 
-	r = close(server_fd);
-	zassert_not_equal(-1, r, "close() failed on the server fd (%d)", errno);
+		recv_len += ret;
+	}
 
-	r = k_thread_join(&server_thread, K_FOREVER);
-	zassert_equal(0, r, "k_thread_join() failed (%d)", r);
+	data = strstr(buf, "\r\n\r\n");
+	zassert_not_null(data, "Header not found");
+
+	data += 4;
+
+	zassert_equal(len, sizeof(compressed_inc_file), "Invalid compressed file size");
+
+	ret = memcmp(data, compressed_inc_file, len);
+	zassert_equal(ret, 0,
+		      "inc_file and compressed_inc_file contents are not identical (%d)", ret);
+
+	ret = zsock_close(client_fd);
+	zassert_not_equal(ret, -1, "close() failed on the client fd (%d)", errno);
+
+	http_server_stop(&ctx);
+
+	ret = k_thread_join(&server_thread, K_FOREVER);
+	zassert_equal(ret, 0, "k_thread_join() failed (%d)", ret);
+
+	ret = http_server_cleanup(&ctx);
+	zassert_equal(ret, 0, "Failed to cleanup server (%d)", ret);
 }
 
 ZTEST(framework_tests_tls, test_tls)
@@ -227,32 +268,36 @@ ZTEST(framework_tests_tls, test_tls)
 
 static void *setup(void)
 {
-	int r;
+	int ret;
 
 	if (IS_ENABLED(CONFIG_TLS_CREDENTIALS)) {
 		NET_DBG("Loading credentials");
-		r = tls_credential_add(HTTP_SERVER_CA_CERTIFICATE_TAG,
-				       TLS_CREDENTIAL_CA_CERTIFICATE, ca, sizeof(ca));
-		zassert_equal(r, 0, "failed to add CA Certificate (%d)", r);
+		ret = tls_credential_add(CA_CERTIFICATE_TAG,
+					 TLS_CREDENTIAL_CA_CERTIFICATE,
+					 ca, sizeof(ca));
+		zassert_equal(ret, 0, "failed to add CA Certificate (%d)", ret);
 
-		r = tls_credential_add(HTTP_SERVER_SERVER_CERTIFICATE_TAG,
-				       TLS_CREDENTIAL_SERVER_CERTIFICATE, server, sizeof(server));
-		zassert_equal(r, 0, "failed to add Server Certificate (%d)", r);
+		ret = tls_credential_add(SERVER_CERTIFICATE_TAG,
+					 TLS_CREDENTIAL_SERVER_CERTIFICATE,
+					 server, sizeof(server));
+		zassert_equal(ret, 0, "failed to add Server Certificate (%d)", ret);
 
-		r = tls_credential_add(HTTP_SERVER_SERVER_CERTIFICATE_TAG,
-				       TLS_CREDENTIAL_PRIVATE_KEY, server_privkey,
-				       sizeof(server_privkey));
-		zassert_equal(r, 0, "failed to add Server Private Key (%d)", r);
+		ret = tls_credential_add(SERVER_CERTIFICATE_TAG,
+					 TLS_CREDENTIAL_PRIVATE_KEY,
+					 server_privkey, sizeof(server_privkey));
+		zassert_equal(ret, 0, "failed to add Server Private Key (%d)", ret);
 
-		r = tls_credential_add(HTTP_SERVER_CLIENT_CERTIFICATE_TAG,
-				       TLS_CREDENTIAL_SERVER_CERTIFICATE, client, sizeof(client));
-		zassert_equal(r, 0, "failed to add Client Certificate (%d)", r);
+		ret = tls_credential_add(CLIENT_CERTIFICATE_TAG,
+					 TLS_CREDENTIAL_SERVER_CERTIFICATE,
+					 client, sizeof(client));
+		zassert_equal(ret, 0, "failed to add Client Certificate (%d)", ret);
 
-		r = tls_credential_add(HTTP_SERVER_CLIENT_CERTIFICATE_TAG,
-				       TLS_CREDENTIAL_PRIVATE_KEY, client_privkey,
-				       sizeof(client_privkey));
-		zassert_equal(r, 0, "failed to add Client Private Key (%d)", r);
+		ret = tls_credential_add(CLIENT_CERTIFICATE_TAG,
+					 TLS_CREDENTIAL_PRIVATE_KEY,
+					 client_privkey, sizeof(client_privkey));
+		zassert_equal(ret, 0, "failed to add Client Private Key (%d)", ret);
 	}
+
 	return NULL;
 }
 
