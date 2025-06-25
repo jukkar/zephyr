887cfbdfcd41615778f916033f69f31ab07dd27a
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2025-06-25 16:26:15 +0300

    tests: net: Allow testing max link address length
    Allow test case to specify the maximum length of the system
    link address. Currently only enabled for IPv6 testing.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- include/zephyr/net/net_linkaddr.h
+++ include/zephyr/net/net_linkaddr.h
@@ -30,7 +30,9 @@ extern "C" {
  */
 
 /** Maximum length of the link address */
-#if defined(CONFIG_MODEM_CELLULAR)
+#if defined(CONFIG_NET_TEST_LINK_ADDR)
+#define NET_LINK_ADDR_MAX_LENGTH CONFIG_NET_TEST_LINK_ADDR_LEN
+#elif defined(CONFIG_MODEM_CELLULAR)
 #define NET_LINK_ADDR_MAX_LENGTH 16
 #elif defined(CONFIG_NET_L2_PHY_IEEE802154) || defined(CONFIG_NET_L2_PPP)
 #define NET_LINK_ADDR_MAX_LENGTH 8
